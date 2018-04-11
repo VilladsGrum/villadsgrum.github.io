c0778fd312fd221ce71df737245771e4a32f3ad3
VilladsGrum <34950354+VilladsGrum@users.noreply.github.com>
2018-04-11 22:08:45 +0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "61451707@sms.telenor.bg";
-$from = "villadsgrum12@gmail.com";
-$message = "This is a text message\nNew line...";
-$headers = "From: ".$from."\n";
-mail($to,'',$message,$headers);
-
-?>
